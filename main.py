10a7757f52e8417f3a0bd78b17800216f5fb734e
fabbeyond <redcraft42.12@gmail.com>
2024-08-23 08:31:32 +0200

    Added GameObjects

--- main.py
+++ main.py
@@ -7,9 +7,10 @@ class Renderer():
         self.root.geometry("800x600")
         self.canvas = tk.Canvas(self.root, width=800, height=600)
         self.canvas.pack()
+        self.focalLength = 1000
         
-    def renderCube(self, focalLength, blockData):
-        x, y, z, size, color = blockData
+    def renderGameObject(self, blockData):
+        type, x, y, z, size, color, attributes = blockData
         proj_list = []
         vertecies = [
             [x-size/2, y-size/2, z-size/2],
@@ -31,8 +32,8 @@ class Renderer():
             ]
 
         for vertex in vertecies:
-            xProj = (focalLength*vertex[0])/(focalLength+vertex[2])
-            yProj = (focalLength*vertex[1])/(focalLength+vertex[2])
+            xProj = (self.focalLength*vertex[0])/(self.focalLength+vertex[2])
+            yProj = (self.focalLength*vertex[1])/(self.focalLength+vertex[2])
             proj_list.append([xProj, yProj])
         
         for index in indices:
@@ -42,9 +43,22 @@ class Renderer():
     def render(self):
         self.canvas.update()
         self.canvas.delete("all")
-        
+
+class GameObject():
+    def __init__(self, type="cube", x=0, y=0, z=0, size=100, color="black", **attributes):
+        self.type = type
+        self.x = x
+        self.y = y
+        self.z = z
+        self.size = size
+        self.color = color
+        self.attributes = attributes
+    def data(self):
+        return [self.type, self.x, self.y, self.z, self.size, self.color, self.attributes]
+
 if __name__ == "__main__":
     renderer = Renderer()
+    block = GameObject("cube", 500, 500, 0, 100, "black").data()
     while True:
-        renderer.renderCube(1000, [100, 100, 0, 100, "black"])
+        renderer.renderGameObject(block)
         renderer.render()
